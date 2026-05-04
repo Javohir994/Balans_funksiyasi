ea68ee0edfacbadae60134b2ea59de2d6fb24eb0
Javohir994 <javohir@example.com>
2026-05-04 15:47:06 +0500

    Smart Shop tizimi kodi qo'shildi

--- test.py
+++ test.py
@@ -1,100 +1,141 @@
-import pandas as pd
 import numpy as np
-banklar = []
-Bank = {"Bank haqida Parol kirtasiz va ism kirtasiz"}
-print(Bank)
-qoshilish = ("Pastagi yerdan ro'yxatdan oting",)
-print(qoshilish)
-#qoshish funksyiyasi
+import pandas as pd
+mahsulotlar = []
+#Aqli magazin haqida
+print('==========SMART SHOP SYSTEM==========')
+haqida = ("Aqli dastur haqida bunda mahsulot qoshasiz va korsangiz ham boladi mahsulotni")
+print(haqida)
+#hozircha bor mahsulotlar
+se = {"Avvalgi narx",}
+print(se)
+raw = np.array([
+    ['Coca cola',8000],
+    ['Non',4000],
+])
+df = pd.DataFrame(raw)
+df = df.set_axis(['Mahsulot', 'Narxi'],axis=1)
+print(df)
+df['Narxi'] = df['Narxi'].astype(int)
+df['Narxi'] = df['Narxi'] - 500
+print(f"Hozir magazinda bor ichimliklar:", raw[0] )
+print(df)
+print("Hozirgi narx")
+df = df.set_index("Mahsulot")
+print(df.loc[['Non', 'Coca cola']])
+#Mahsulot qo'shish funksiyasi
 def qosh():
-    ism = input("Ism :")
-    parol = int(input("Parol (faqat son) :"))
-    balans = int(input("Balans :"))
-    royxat = {"Ism": ism, "parol":parol, "balans":balans}
-    banklar.append(royxat)
-#login funksiyasi
-def login():
-    ism_k = input("Ism :")
-    parol_k = int(input("Parol :"))
-    topildi = False
-    for foydalanuvchi in banklar:
-        if foydalanuvchi['Ism'] == ism_k:
-            topildi = True
-            if foydalanuvchi['parol'] == parol_k:
-                print(f"Parol to'gri balansingiz : {foydalanuvchi['balans']}")
-            else:
-                print("Parol xato!")
-                return
-            if not topildi:
-                print("Bunday foydalanuvchi topilmadi!")
-#korish funksyiasi
+    nomi = input("Mahsulot nomi :")
+    narxi = int(input("Narxi :"))
+    soni = int(input("Soni :"))
+    kotegor = input("Kotegoryasi :")
+    mahsulot = {'Nomi': nomi, 'Narxi': narxi, 'Soni': soni, 'Kotegoryasi': kotegor}
+    mahsulotlar.append(mahsulot)
+    print(f"Mahsulot {nomi} qo'shildi")
+#Ko'rish funksiyasi
 def korish(data):
     if not data:
-        print("Hozircha balans yo'q!")
+        print("Savat bo'sh")
         return
-    for b in banklar:
-        print(f"Ism: {b['Ism']}, {b['parol']}, {b['balans']}")
-#eng kop balans
+    for m in mahsulotlar:
+        print({'Nomi': m['Nomi'], 'Narxi': m['Narxi'], 'Soni': m['Soni'], 'Kotegoryasi': m['Kotegoryasi']})
+#qidirish funksiyasi
+def qidir():
+    nom_k = input("Mahsulot nomi :")
+    topildi = False
+    for foydalanuvchi in mahsulotlar:
+        if foydalanuvchi['Nomi'] == nom_k:
+            topildi = True
+            narxi = foydalanuvchi['Narxi']
+            soni = foydalanuvchi['Soni']
+            kateg = foydalanuvchi['Kotegoryasi']
+            print(f"Mahsulot topildi! Narxi: {narxi}, Soni: {soni}, Kategoriyasi: {kateg}")
+            break 
+    if not topildi:
+        print("Bunday mahsulot topilmadi !")
+#mahsulot yangilash funksiyasi
+def yangilash():
+    nom_k = input("Mahsulot nomi :")
+    topildi = False
+    for m in mahsulotlar:
+        if m['Nomi'] == nom_k:
+            topildi = True
+            angi_narx = int(input(f"{nom_k} uchun yangi narx: "))
+            yangi_soni = int(input(f"{nom_k} uchun yangi soni: "))
+            m['Narxi'] = angi_narx
+            m['Soni'] = yangi_soni
+            print(f"Muvaffaqiyatli yangilandi: {nom_k}")
+            break # Topilgach, siklni to'xtatamiz
+    if not topildi:
+        print("Bunday mahsulot topilmadi!")
+#sotuv funksiyasi
+def sotuv():
+    nom = input("Mahsulot nomi :")
+    topildi = False
+    for m in mahsulotlar:
+        if m['Nomi'] == nom:
+            topildi = True
+            nechta = int(input(f"Nechta {nom} sotib olasiz :"))
+            if nechta < m['Soni']:
+                jami = m['Narxi'] * nechta
+                m['Soni'] -= nechta
+            print(f"Sotuv bajarildi!")
+            print(f"Jami summa: {jami} so'm")
+            print(f"Omborda qoldi: {m['Soni']} ta")
+        else:
+            print(f"Xato! Omborda bor-yog'i {m['Soni']} ta mahsulot bor.")
+            break    
+    if not topildi:
+        print("Kechirasiz, bunday mahsulot bizda yo'q.")
+#eng qimmat tavarni korish
 def qimmat(data):
     if not data:
-        print(f"Hozircha balans yo'q!")
+        print("Savat bo'sh")
         return
-    eng = max(data, key=lambda x: x['balans'])
-    print(f"Eng qimmat Balans egasi :{eng['Ism']} balabsi :{eng['balans']}")
-#ortacha balans funksiyasi
-if banklar:
-    df = pd.DataFrame(banklar)
-    ortacha = np.mean(df[['Ism', 'parol', 'balans']])
-    print(f"O'rtacha Balans :{ortacha}")
-#Ozgartirish funksiyasi
-    df = df.set_axis(['Ism', 'parol', 'balans'], axis=1)
-    print(df)
-    df['balans'] = df['balans'] + 5000
-# numpy array
-    arr = np.array([
-        'alisher usmonov', 1400000000,
-        'Jahongiz ortiqhojayev', 800000000,
-        'Gulnora Karimova', 600000000,
-    ])
-    print(f"Bizga kiredt qoygan mashxur inson :{arr[1]}")
-    print(f"Siz balansingizga 5000 so'm qoshildi {df['balans']}")
-#wile true orqali menyu
+    eng = max(data, key=lambda x:x['Narxi'])
+    return f"Eng qimmat mahsulot narxi {eng['Nomi']}, Narxi: {eng['Narxi']} so'm"
+#statistikani korish
+def ststis(data):
+    if not data:
+        print("statistika uchun ma'lumot yetarli emas")
+        return
+    temp_df = pd.DataFrame(data)
+    ortacha = np.mean(temp_df['Narxi'])
+    jami = np.sum(temp_df['Soni'])
+    print("========DO'KON STATISTIKA========")
+    print(f"Ombrdagi jami mahsulot soni: {jami}")
+    print(f"Mahsulotlarni o'rtacha narxi: {ortacha}")
+    print("=================================")
+#loop funksiyasi
 while True:
-    print("1. Royxatdan o'tish")
-    print("2. Login")
-    print("3. Balansni ko'rish")
-    print("4. Eng qimmat balans")
-    print("5. O'rtacha balans")
-    print("6. CHiqish")
+#dastur qoidasi
+    print("1 Mahsulot qoshish")
+    print("2 Mahsulot ko'rish")
+    print("3 qidirish")
+    print("4 yangilash")
+    print("5 sotuv qilish")
+    print("6 eng qimmat mahsulot ko'rish")
+    print("7 statistika")
+    print("8 chiqish")
     tanlov = int(input("Tanlov :"))
-#if elif else orqali tanlov
     if tanlov == 1:
         qosh()
     elif tanlov == 2:
-        login()
+        korish(mahsulotlar)
     elif tanlov == 3:
-        korish(banklar)
+        qidir()
     elif tanlov == 4:
-        qimmat(banklar)
-    elif tanlov == 5:
-        if banklar:
-            df = pd.DataFrame(banklar)
-            ortacha = np.mean(df['balans'])
-            arr = np.array([
-        'alisher usmonov', 1400000000,
-        'Jahongiz ortiqhojayev', 800000000,
-        'Gulnora Karimova', 600000000,
-    ])
-            print(f"O'rtacha Balans :{ortacha}")
-            print(f"Bizga kiredt qoygan mashxur inson :{arr[1]}")
-            df['balans'] = df['balans'] + 5000
-            print(f"Siz balansingizga 5000 so'm qoshildi {df['balans']}")
-            print(df['balans'])
-        else:
-            print("Hozircha balans yo'q!")
+        yangilash()
+    elif tanlov == 5: 
+        sotuv()
     elif tanlov == 6:
-        print("Dasturdan chiqildi!")
+        qimmat(mahsulotlar)
+    elif tanlov == 7:
+        ststis(mahsulotlar)
+    elif tanlov == 8:
+        print("Dasturdan chiqildi xayr")
         break
     else:
-        print("Noto'g'ri tanlov, qayta urinib ko'ring!")
+        print("Xato son kiriting!")
+#csvga saqalash
+    df.to_csv('talaba.csv', index=False)
 
