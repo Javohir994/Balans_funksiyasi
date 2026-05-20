6cf51b495dd60adcc74463fc72f89ef9239f7723
Javohir994 <javohir@example.com>
2026-05-20 19:49:57 +0500

    Streamlit loyiham to'liq kodi

--- from sklearn.py
+++ from sklearn.py
@@ -1,26 +1,27 @@
-import psycopg2
+import pandas as pd
+import numpy as np
 
-# 1. Bazaga ulanish
-conn = psycopg2.connect(
-    dbname="bazangiz_nomi", 
-    user="postgres", 
-    password="parolingiz", 
-    host="localhost"
-)
-cursor = conn.cursor()
+from sklearn.linear_model import LinearRegression
 
-# 2. SQL buyrug'ini matn (string) ko'rinishida yozish
-# E'tibor bering: Python'da matn ichida (') belgisi bo'lsa, 
-# tashqaridan uchta qo'shtirnoq """ ishlatish xavfsizroq.
-sql_query = """
-INSERT INTO kitoblar (nomi, muallif, narxi, mavjudmi)
-VALUES ('O''tkan kunlar', 'Abdulla Qodiriy', 45000, true);
-"""
+data = {
+    'masofa': [1, 2, 3, 4, 5],
+    'narx': [100, 200, 300, 400, 500]
+}
+df = pd.DataFrame(data)
+x = df['masofa'].values.reshape(1,-1)
+y = df['narx'].values
+# 2. Bo'sh model yaratamiz
+ai_model = LinearRegression()
 
-# 3. Buyruqni bajarish
-cursor.execute(sql_query)
-conn.commit()
+# 3. Modelni o'qitamiz (fit funksiyasi - o'rganish buyrug'i)
+ai_model.fit(x, y)
+print("Ajoyib! Sun'iy intellekt ma'lumotlarni o'rgandi va qoidani topdi.")
+# 4. Yangi masofalarni beramiz (ustun ko'rinishida)
+yangi_mijoz = pd.DataFrame({'Masofa': [30, 50]})
 
-# 4. Ulanishni yopish
-cursor.close()
-conn.close()
+# 5. Bashorat qilish (predict funksiyasi)
+javoblar = ai_model.predict(yangi_mijoz)
+
+print("\n--- AI BASHORATI ---")
+print(f"30 km uchun narx: {int(javoblar[0])} so'm")
+print(f"50 km uchun narx: {int(javoblar[1])} so'm")
--- test.py
+++ test.py
@@ -1,141 +1,141 @@
-import numpy as np
 import pandas as pd
-mahsulotlar = []
-#Aqli magazin haqida
-print('==========SMART SHOP SYSTEM==========')
-haqida = ("Aqli dastur haqida bunda mahsulot qoshasiz va korsangiz ham boladi mahsulotni")
-print(haqida)
-#hozircha bor mahsulotlar
-se = {"Avvalgi narx",}
+import numpy as np
+#Dastur nomi
+print("=====SCHOOL MANAGEMENT SYSTEM=====")
+#bosh list
+oquvchilar = []
+#malumot 1
+df1 = pd.DataFrame({
+    "Ism_1":["Ali"],
+    "Sinf_1":["1A"]
+})
+#malumot 2
+df2 = pd.DataFrame({
+    "Ism_2":["Vali"],
+    "Sinf_2":["1G"]
+})
+#merge
+df = pd.merge(df1, df2, left_on="Ism_1", right_on="Ism_2", how="outer")
+#groupby
+d1 = df.groupby("Sinf_2")["Sinf_1"].mean()
+print(d1)
+#fillna
+d = df.fillna({
+    "Ism_1":"Gani",
+    "Sinf_1":"1B",
+    "Ism_2":"Soli",
+    "Sinf_2":"1V"
+})
+#tuple
+tup = ("Avvalgi o'quvchilar",)
+print(tup)
+#concat va axis
+dw = pd.concat([df1,df2,],axis=1)
+print(dw)
+#set
+se = {"Hozirgi o'quvchilar"}
 print(se)
-raw = np.array([
-    ['Coca cola',8000],
-    ['Non',4000],
-])
-df = pd.DataFrame(raw)
-df = df.set_axis(['Mahsulot', 'Narxi'],axis=1)
-print(df)
-df['Narxi'] = df['Narxi'].astype(int)
-df['Narxi'] = df['Narxi'] - 500
-print(f"Hozir magazinda bor ichimliklar:", raw[0] )
-print(df)
-print("Hozirgi narx")
-df = df.set_index("Mahsulot")
-print(df.loc[['Non', 'Coca cola']])
-#Mahsulot qo'shish funksiyasi
+print(d)
+#def qoshish
 def qosh():
-    nomi = input("Mahsulot nomi :")
-    narxi = int(input("Narxi :"))
-    soni = int(input("Soni :"))
-    kotegor = input("Kotegoryasi :")
-    mahsulot = {'Nomi': nomi, 'Narxi': narxi, 'Soni': soni, 'Kotegoryasi': kotegor}
-    mahsulotlar.append(mahsulot)
-    print(f"Mahsulot {nomi} qo'shildi")
-#Ko'rish funksiyasi
+    ism = input("Student ismi:")
+    yosh = int(input("Yoshi:"))
+    sinf = input("Sinf:")
+    oquvchi = {"Ism":ism, "Yosh":yosh, "Sinf":sinf}
+    oquvchilar.append(oquvchi)
+    print("✅ Student qo'shildi")
+#techer qoshish
+def teach():
+    teac = input("Teacher:")
+    fan = input("Fan:")
+    oqituvchi = {"Teacher":teac, "Fan":fan}
+    oquvchilar.append(oqituvchi)
+    print("✅ Teacher qo‘shildi")
+#davomat
+def dova():
+    ism = input("Ismi :")
+    holat = input("Holati :")
+    davomat = {"Ismi":ism, "Holat":holat}
+    oquvchilar.append(davomat)
+    print("✅ Davomat saqlandi")
+#exam
+def exam():
+    ism = input("Ismi :")
+    fan1 = int(input("Matematika :"))
+    fan2 = int(input("English :"))
+    fan3 = int(input("Fizika :"))
+    exzamin = {"Ism":ism, "Matematika":fan1, "English":fan2, "Fizika":fan3}
+    oquvchilar.append(exzamin)  
+    print("✅ Natija saqlandi")
+#korish funksiyasi
 def korish(data):
     if not data:
-        print("Savat bo'sh")
+        print("Savat bosh!")
         return
-    for m in mahsulotlar:
-        print({'Nomi': m['Nomi'], 'Narxi': m['Narxi'], 'Soni': m['Soni'], 'Kotegoryasi': m['Kotegoryasi']})
-#qidirish funksiyasi
-def qidir():
-    nom_k = input("Mahsulot nomi :")
-    topildi = False
-    for foydalanuvchi in mahsulotlar:
-        if foydalanuvchi['Nomi'] == nom_k:
-            topildi = True
-            narxi = foydalanuvchi['Narxi']
-            soni = foydalanuvchi['Soni']
-            kateg = foydalanuvchi['Kotegoryasi']
-            print(f"Mahsulot topildi! Narxi: {narxi}, Soni: {soni}, Kategoriyasi: {kateg}")
-            break 
-    if not topildi:
-        print("Bunday mahsulot topilmadi !")
-#mahsulot yangilash funksiyasi
-def yangilash():
-    nom_k = input("Mahsulot nomi :")
-    topildi = False
-    for m in mahsulotlar:
-        if m['Nomi'] == nom_k:
-            topildi = True
-            angi_narx = int(input(f"{nom_k} uchun yangi narx: "))
-            yangi_soni = int(input(f"{nom_k} uchun yangi soni: "))
-            m['Narxi'] = angi_narx
-            m['Soni'] = yangi_soni
-            print(f"Muvaffaqiyatli yangilandi: {nom_k}")
-            break # Topilgach, siklni to'xtatamiz
-    if not topildi:
-        print("Bunday mahsulot topilmadi!")
-#sotuv funksiyasi
-def sotuv():
-    nom = input("Mahsulot nomi :")
-    topildi = False
-    for m in mahsulotlar:
-        if m['Nomi'] == nom:
-            topildi = True
-            nechta = int(input(f"Nechta {nom} sotib olasiz :"))
-            if nechta < m['Soni']:
-                jami = m['Narxi'] * nechta
-                m['Soni'] -= nechta
-            print(f"Sotuv bajarildi!")
-            print(f"Jami summa: {jami} so'm")
-            print(f"Omborda qoldi: {m['Soni']} ta")
-        else:
-            print(f"Xato! Omborda bor-yog'i {m['Soni']} ta mahsulot bor.")
-            break    
-    if not topildi:
-        print("Kechirasiz, bunday mahsulot bizda yo'q.")
-#eng qimmat tavarni korish
-def qimmat(data):
+    for oquv in oquvchilar:
+        print({"Ism":[oquv], "Yosh":[oquv], "Sinf":[oquv]})
+#ranking funksiyasi
+def rank(df):
+    if "Ball" not in df.columns:
+        print("❌ Ball yo‘q")
+        return
+    sorted_df = df.sort_values(by="Ball", ascending=False)
+    print("===== RANKING =====")
+    for i, row in enumerate(sorted_df.itertuples(), start=1):
+        print(i, row.Ism, row.Ball)
+#statis
+def statis(data):
     if not data:
-        print("Savat bo'sh")
+        print("Savat bo'sh!")
         return
-    eng = max(data, key=lambda x:x['Narxi'])
-    return f"Eng qimmat mahsulot narxi {eng['Nomi']}, Narxi: {eng['Narxi']} so'm"
-#statistikani korish
-def ststis(data):
+    all_score = df[["Matematika", "English", "Fizika" ]]
+    print("=============================")
+    print("O'rtacha:", np.mean(all_score))
+    print("Eng ko'p:", np.max(all_score))
+    print("Eng kam:", np.min(all_score))
+    print("=============================")
+#csvga saqlash
+def csv(data):
     if not data:
-        print("statistika uchun ma'lumot yetarli emas")
+        print("Savat bo'sh!")
         return
-    temp_df = pd.DataFrame(data)
-    ortacha = np.mean(temp_df['Narxi'])
-    jami = np.sum(temp_df['Soni'])
-    print("========DO'KON STATISTIKA========")
-    print(f"Ombrdagi jami mahsulot soni: {jami}")
-    print(f"Mahsulotlarni o'rtacha narxi: {ortacha}")
-    print("=================================")
-#loop funksiyasi
+    dg = pd.DataFrame(data)
+    dg.to_csv("talaba.csv", index=False)
+#loop
 while True:
-#dastur qoidasi
-    print("1 Mahsulot qoshish")
-    print("2 Mahsulot ko'rish")
-    print("3 qidirish")
-    print("4 yangilash")
-    print("5 sotuv qilish")
-    print("6 eng qimmat mahsulot ko'rish")
+    print("======================")
+#qoidasi
+    print("1 qo'shish")
+    print("2 teacher qo'shish")
+    print("3 Davomat qo'shish")
+    print("4 examin natija qo'shish")
+    print("5 studentlarni ko'rish")
+    print("6 Ranking")
     print("7 statistika")
-    print("8 chiqish")
-    tanlov = int(input("Tanlov :"))
+    print("8 csvga saqlash")
+    print("9 chiqish")
+    print("======================")
+#tanlash
+    tanlov = int(input("Tanlang :"))
+#if elif else funksiya
     if tanlov == 1:
         qosh()
     elif tanlov == 2:
-        korish(mahsulotlar)
+        teach()
     elif tanlov == 3:
-        qidir()
+        dova()
     elif tanlov == 4:
-        yangilash()
-    elif tanlov == 5: 
-        sotuv()
+        exam()
+    elif tanlov == 5:
+        korish(oquvchilar)
     elif tanlov == 6:
-        qimmat(mahsulotlar)
+        rank(oquvchilar)
     elif tanlov == 7:
-        ststis(mahsulotlar)
+        statis(oquvchilar)
     elif tanlov == 8:
-        print("Dasturdan chiqildi xayr")
+            csv()
+    elif tanlov == 9:
+        print("xayr!")
         break
     else:
-        print("Xato son kiriting!")
-#csvga saqalash
-    df.to_csv('talaba.csv', index=False)
-
+        print("Xato!")
